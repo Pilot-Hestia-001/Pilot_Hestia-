3703cb606ee03efc4913be1e327d584dc5552f5c
passwordd009 <elijah.hawes47@gmail.com>
2025-08-08 05:32:39 -0400

    changed urls

--- Hestia_Frontend/vite.config.js
+++ Hestia_Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: './', 
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': 'https://pilot-hestia-backend.onrender.com'
     }
   },
 
