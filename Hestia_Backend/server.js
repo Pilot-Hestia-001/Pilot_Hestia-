e9fb00d17b33b0c2bd7ab4d4c56550e274b2e0ee
passwordd009 <elijah.hawes47@gmail.com>
2025-08-08 05:19:17 -0400

    quick fix

--- Hestia_Backend/server.js
+++ Hestia_Backend/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Adjust as needed
+    origin: 'https://pilot-hestia-frontend.onrender.com', // Adjust as needed
     methods: ['GET', 'POST']
   }
 });
@@ -113,7 +113,9 @@ io.on('connection', (socket) => {
 
 app.use(express.json()); 
 app.use(cors({
-    origin: '*'
+    origin: 'https://pilot-hestia-frontend.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   }));
 
 //test route
