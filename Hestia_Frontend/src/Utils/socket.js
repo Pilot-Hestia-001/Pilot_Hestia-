d0e3cd8c4a45c431a1fbad0451cee1447577ee4c
passwordd009 <elijah.hawes47@gmail.com>
2025-08-08 05:24:49 -0400

    changed urls

--- Hestia_Frontend/src/Utils/socket.js
+++ Hestia_Frontend/src/Utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://pilot-hestia-backend.onrender.com", {
   auth: {
     token: localStorage.getItem('token'), // or however you store it
   },
