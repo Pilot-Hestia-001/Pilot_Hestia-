d2514004c0a722b4c37c7c88f2d1f6fcf7241eb2
passwordd009 <elijah.hawes47@gmail.com>
2025-08-10 17:42:48 -0400

    made deployment and dev adjustments

--- Hestia_Frontend/vite.config.js
+++ Hestia_Frontend/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', 
   server: {
     proxy: {
-      '/api': 'https://pilot-hestia-backend.onrender.com'
+      '/api': 'http://localhost:8080'
     }
   },
 
